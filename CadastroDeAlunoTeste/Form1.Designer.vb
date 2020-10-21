f36385d623ee1d08a858da36d22e589ad967dfaf
GiovanniDeLuca7 <giovannidemonst@gmail.com>
2020-10-21 10:05:46 -0300

    aperfeiçoamento

--- CadastroDeAlunoTeste/Form1.Designer.vb
+++ CadastroDeAlunoTeste/Form1.Designer.vb
@@ -486,7 +486,7 @@ Partial Class Form1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(556, 93)
+        Me.Button1.Location = New System.Drawing.Point(547, 6)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 37
@@ -499,7 +499,7 @@ Partial Class Form1
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.AutoSize = true
         Me.BackColor = System.Drawing.Color.BlueViolet
-        Me.ClientSize = New System.Drawing.Size(870, 500)
+        Me.ClientSize = New System.Drawing.Size(870, 347)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.lbl_aviso)
         Me.Controls.Add(Me.cmd_limpar)
